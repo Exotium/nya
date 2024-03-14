fa9e80b13e9d379dd68425930cb528a25b91e0a8
Exotium <103858526+Exotium@users.noreply.github.com>
2024-03-14 13:55:33 +0100

    meow mrrrp

---
+++ README.md
@@ -0,0 +1,2 @@
+my first ever attempt at some compression
+it's not very compact but it's smaller than the text it prints, which was my goal
---
+++ hanna.c
@@ -0,0 +1,4 @@
+i=0;s;main(){for(;s="UjVijVijVejUZUjVijViZgj]jvYgj]gZgv]j]"
+"jvYg^kzmzmzmZwm^k]wzmzmzmswskswsgN{mszlwNkswswljszljNkszL"
+"cNksLzljNshJcrhjLJcJshLjLcrhjL@jBh@jJ`BhJ@J`j@h@jJ`"[i/3];
+++i){if(!(i%62))puts("");putchar("#: +"[(s>>(i%3)*2)&3]);}}
