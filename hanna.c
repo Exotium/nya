238a8a3939e02e8516b9bf6ea7ec80ae8447f3dd
Exotium <103858526+Exotium@users.noreply.github.com>
2024-03-14 19:40:14 +0100

    shave off 12 bytes

--- hanna.c
+++ hanna.c
@@ -1,4 +1,4 @@
-i=0;s;main(){for(;s="UjVijVijVejUZUjVijViZgj]jvYgj]gZgv]j]"
-"jvYg^kzmzmzmZwm^k]wzmzmzmswskswsgN{mszlwNkswswljszljNkszL"
-"cNksLzljNshJcrhjLJcJshLjLcrhjL@jBh@jJ`BhJ@J`j@h@jJ`"[i/3];
-++i){if(!(i%62))puts("");putchar("#: +"[(s>>(i%3)*2)&3]);}}
+i;main(s){for(;s="UjVijVijVejUZUjVijViZgj]jvYgj]gZgv]j]"
+"jvYg^kzmzmzmZwm^k]wzmzmzmswskswsgN{mszlwNkswswljszljNk"
+"szLcNksLzljNshJcrhjLJcJshLjLcrhjL@jBh@jJ`BhJ@J`j@h@jJ`"
+[i/3];)i%62?0:puts(""),putchar("#: +"[(s>>i++%3*2)&3]);}
